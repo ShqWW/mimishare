311e029742f4397fc186ca2e399c1b7d4803f03b
wsq <wsq@MyNAS>
2024-10-07 22:22:37 +0800

    update

--- main.py
+++ main.py
@@ -50,5 +50,5 @@ app.post("/admin")(update_config)
 # 运行应用
 if __name__ == "__main__":
     
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=4066)
     
